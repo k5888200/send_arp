c58fc3e4ff773ebf2204fcbbcdc32910502d5fb8
k5888200 <k5888200@naver.com>
2017-10-09 15:54:34 +0900

    Add GetLocalIP/HA

--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
-#include <pcap.h>
-#include <stdio.h>
-#include <arpa/inet.h>
-#include <netinet/if_ether.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <ifaddrs.h>
-#include <bits/stdc++.h>
-
-
-
-void usage() {
-	printf("syntax: pcap_test <interface> <send ip> <target ip>\n");
-	printf("sample: pcap_test wlan0 192.168.10.2 192.168.10.1\n");
-}
+#include "ty_network.h"
 
 int main(int argc, char* argv[]) {
-	if (0 && argc != 4) {
-		usage();
-		return -1;
-	}
+	my_assert(argc == 4, "syntax: %s <interface> <send ip> <target ip>\n", argv[0]);
 
-	struct ifaddrs *ifap, *ifa;
-	struct sockaddr_in *sa;
-	char *addr;
+	in_addr attack_ip, sender_ip, target_ip;
+	ether_addr attack_ha, sender_ha;
 
-	getifaddrs (&ifap);
-	for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
-		if (ifa->ifa_addr->sa_family==AF_INET) {
-			sa = (struct sockaddr_in *) ifa->ifa_addr;
-			addr = inet_ntoa(sa->sin_addr);
-			printf("Interface: %s\tAddress: %s\n", ifa->ifa_name, addr);
-		}
-	}
+	my_assert( GetLocalIP(&attack_ip, argv[1]) , "Error On Getting IP Address");
 
-	freeifaddrs(ifap);
+	my_assert( GetLocalHA(&attack_ha, argv[1]), "Error On Getting Hardware Address");
+
+	for(int i=0;i<6;i++) printf("%02x ",attack_ha.ether_addr_octet[i]); printf("\n");
+
 	return 0;
 }
