8241c5160a6085d1b6ed20011a90508146a1299b
k5888200 <k5888200@naver.com>
2017-10-11 16:00:34 +0900

    Change argv[1] to interface

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 int main(int argc, char* argv[]) {
 	my_assert(argc == 4, "syntax: ./%s <interface> <send ip> <target ip>\n", argv[0]);
 
+	const char *interface = argv[1];
+
 
 	struct in_addr 		attack_ip, sender_ip, target_ip;
 	struct ether_addr 	attack_ha, sender_ha, target_ha;
 
-	printf("- Interface: %s\n",argv[1]);
+	printf("- Interface: %s\n",interface);
 	my_assert( inet_pton(AF_INET, argv[2], &sender_ip) > 0, "Invalid sender_ip Or Error On Copying IP");
 	my_assert( inet_pton(AF_INET, argv[3], &target_ip) > 0, "Invalid target_ip Or Error On Copying IP");
 
-	my_assert( GetLocalIP(&attack_ip, argv[1]), "Error On Getting Local IP Address\n");
+	my_assert( GetLocalIP(&attack_ip, interface), "Error On Getting Local IP Address\n");
 	printf("- Local_IP: %s\n", inet_ntoa(attack_ip));
-	my_assert( GetLocalHA(&attack_ha, argv[1]), "Error On Getting Local Hardware Address\n");
+	my_assert( GetLocalHA(&attack_ha, interface), "Error On Getting Local Hardware Address\n");
 	printf("- Local_HA: %s\n", usr_ether_ntoa(&attack_ha));
 
 	char errbuf[PCAP_ERRBUF_SIZE];
